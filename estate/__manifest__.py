da5a19c498e0efe24f74a230c83cb85706786c1e
Serhii Y <serhiy.yasko@yahoo.co.uk>
2025-01-14 14:36:46 +0200

    Define access rights on Real Estate app model

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,9 @@
     'depends': [
         'base',
     ],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
     'installable': True,
     'application': True,
     'auto_install': False
