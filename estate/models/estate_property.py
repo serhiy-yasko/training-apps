95be0f4490fdcfaf8da80dd9fb9e3ca764360d09
Serhii Y <serhiy.yasko@yahoo.co.uk>
2025-01-14 16:45:14 +0200

    Add menus and extra fields to Real Estate app

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,23 +1,29 @@
 from odoo import fields, models
 
 
-class RecurringPlan(models.Model):
+class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate advertisement"
-    _order = "sequence"
 
     name = fields.Char('Title', required=True)
     description = fields.Text('Description')
-    date_availability = fields.Date('Availability Date')
+    date_availability = fields.Date(
+        'Available From', copy=False,
+        default=fields.Date.to_date(
+            f"{fields.Date.today().year}-{fields.Date.today().month+3}-{fields.Date.today().day}"))
     postcode = fields.Char('Postcode')
-    bedrooms = fields.Integer('Bedrooms')
+    bedrooms = fields.Integer('Bedrooms', default=2)
     living_area = fields.Integer('Living Area (sqm)')
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         string='Garden Orientation')
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'),
+                   ('sold', 'Sold'), ('cancelled', 'Cancelled')], default='new')
