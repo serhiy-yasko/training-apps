6e624e6d97cbc7b63bf15181f5c04f47c9b2aa11
Serhii Y <serhiy.yasko@yahoo.co.uk>
2025-01-14 11:25:24 +0200

    Add Real Estate app

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Category',
+    'summary': 'Manage real estate advertisement',
+    'description': "",
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
