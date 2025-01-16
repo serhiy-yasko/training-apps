79d3521935ac0d08ec47353071e1be3ad9e9fff7
Serhii Y <serhiy.yasko@yahoo.co.uk>
2025-01-16 16:57:24 +0200

    Add computed fields to Estate Property Offer model

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from datetime import timedelta
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -9,3 +10,18 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_deadline', inverse="_inverse_deadline")
+
+    @api.depends('create_date', 'validity')
+    def _compute_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + timedelta(days=record.validity)
+            else:
+                record.date_deadline = fields.Date.today() + timedelta(days=record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
+            record.create_date = record.date_deadline - timedelta(days=record.validity)
